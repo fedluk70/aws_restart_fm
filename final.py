d4a415b0a023e9d5be7c54b5cfd336540beb1ff8
fedluk70 <92407919+fedluk70@users.noreply.github.com>
2022-08-23 02:44:07 +0300

    Add files via upload

---
+++ final.py
@@ -0,0 +1,7 @@
+
+name = input("What is your name? ")
+color = input("What is your fav color? ")
+animal = input("What is your fav animal? ")
+print(name + ", you like " + color + " " + animal + "!")
+print("==============================================")
+print("{}, you like a {} {}!".format(color,name,animal))
---
+++ list.py
@@ -0,0 +1,10 @@
+
+print("Please enter the values")
+name=input("What is your name? ")
+print("Thanks " + name)
+age=input("What is your age? ")
+print(name + " is " + age + " years old" )
+item1=input("Cost of Item 1 ") 
+item2=input("Cost of Item 2 ")
+print("The item 1 is " + item1 + " and item 2 is " + item2)
+
